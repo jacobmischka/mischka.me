fc905440d59bf86085b58f4a0a1a8ab3bdccf649
Jacob Mischka <jacob@mischka.me>
2021-02-27 02:45:41 -0600

    Initial commit, add scaffolding

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = {
+	templateFormats: [
+		'html',
+		'md',
+		'njk',
+		'liquid',
+		'css',
+		'jpg',
+		'png',
+		'webp',
+		'mp4'
+	],
+	dir: {
+		includes: '_includes',
+		layouts: '_layouts'
+	}
+};
