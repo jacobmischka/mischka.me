e0cc77ced82120ee4c3c281e60115c5112943c2d
Jacob Mischka <jacob@mischka.me>
2021-07-25 13:28:14 -0500

    Fix post dates, both on post and blog list

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = eleventyConfig => {
 	eleventyConfig.addShortcode('formatDate', date => {
-		return dateFormatter.format(date);
+		// Hack to work around explicit dates being UTC and implicit dates being local
+		const formatter =
+			date.getMinutes() === 0 && date.getSeconds() === 0
+				? utcDateFormatter
+				: dateFormatter;
+		return formatter.format(date);
 	});
 
 	return {
@@ -14,16 +19,22 @@ module.exports = eleventyConfig => {
 			'jpg',
 			'png',
 			'webp',
-			'mp4'
+			'mp4',
 		],
 		dir: {
 			includes: '_includes',
-			layouts: '_layouts'
-		}
+			layouts: '_layouts',
+		},
 	};
 };
 
 const dateFormatter = new Intl.DateTimeFormat('en-US', {
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+});
+
+const utcDateFormatter = new Intl.DateTimeFormat('en-US', {
 	timeZone: 'UTC',
 	year: 'numeric',
 	month: 'long',
